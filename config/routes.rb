97a1511acf06aff129970efe033332847997b34d
chinmairam <chinmairammuppa@gmail.com>
2020-04-06 02:15:27 +0530

    Use the resources routing coconvention for Todos

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "todos", to: "todos#index"
-  get "todos/:id", to: "todos#show"
-
-  get "todos/create",to: "todos#create"
+  resources :todos
 end
