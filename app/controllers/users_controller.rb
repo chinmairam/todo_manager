745bdaf73de926f4ad467ace73ab4012fdcb4d05
chinmairam <chinmairammuppa@gmail.com>
2020-04-06 04:10:49 +0530

    show method added to display emails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
     def index
-        render plain: User.all.to_a
+        render plain: User.all.map{ |user| user.to_s }.join("\n")
+    end
+
+    def show
+        user=User.find(params[:id])
+        render plain: user.to_s
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+    def to_s
+        "#{id}, #{user_name}, #{email}"
+    end
 end
