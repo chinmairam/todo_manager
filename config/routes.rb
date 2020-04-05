9a765eeb77a5e9717617ac72fe4ef711285aaccd
chinmairam <chinmairammuppa@gmail.com>
2020-04-06 01:55:32 +0530

    Add ability to create a new Todo

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # RESOURCE
-  # Todo is a resource.
-  # - list of resources can be seen: index
-  # - single resource can be seen: show
-  # - create aa new resource
   get "todos", to: "todos#index"
   get "todos/:id", to: "todos#show"
+
+  get "todos/create",to: "todos#create"
 end
