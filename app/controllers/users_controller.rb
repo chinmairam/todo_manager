043b8f29f147b3fc6a92eaf06472da16f1f391cd
chinmairam <chinmairammuppa@gmail.com>
2020-04-06 11:53:18 +0530

    Login added,Target completed,Code Formatted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,36 @@
 class UsersController < ApplicationController
-    skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
-    def index
-        render plain: User.all.map { |user| user.to_s }.join("\n")
-    end
+  def index
+    render plain: User.all.map { |user| user.to_s }.join("\n")
+  end
 
-    def show
-        id = params[:id]
-        user = User.find(id)
-        render plain: user.to_s
-    end
+  def show
+    id = params[:id]
+    user = User.find(id)
+    render plain: user.to_s
+  end
 
-    def create
-        user_name = params[:user_name]
-        user_email = params[:user_email]
-        user_password = params[:user_password]
-        new_user = User.create!(
-            user_name: user_name,
-            user_email: user_email,
-            user_password: user_password,
-        )
-        render plain: "User created successfully with #{new_user.id}"
-    end
+  def create
+    user_name = params[:user_name]
+    user_email = params[:user_email]
+    user_password = params[:user_password]
+    new_user = User.create!(
+      user_name: user_name,
+      user_email: user_email,
+      user_password: user_password,
+    )
+    render plain: "User created successfully with #{new_user.id}"
+  end
 
-    def login
-        check1 = params[:user_email]
-        check2 = params[:user_password]
-        user = User.find { |u| u.user_email == check1 }
-        if user.user_password == check2
-          render plain: "Your login is successful!! Welcome #{user.user_name}"
-        else
-          render plain: "Incorrect credentials!! Try again"
-        end
+  def login
+    check1 = params[:user_email]
+    check2 = params[:user_password]
+    user = User.find { |u| u.user_email == check1 }
+    if user.user_password == check2
+      render plain: "Your login is successful!! Welcome #{user.user_name}"
+    else
+      render plain: "Incorrect credentials!! Try again"
     end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org
   
-  #get "todos", to: "todos#index"
-  #post "todos/create", to: "todos#create"
-  #get "todos/:id", to: "todos#show"
+  # get "todos", to: "todos#index"
+  # post "todos/create", to: "todos#create"
+  # get "todos/:id", to: "todos#show"
   
   resources :todos
   resources :users
