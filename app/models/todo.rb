449a265aac3b39ce9f00914197b78329d60dcfe6
chinmairam <chinmairammuppa@gmail.com>
2020-04-06 21:01:55 +0530

    Login added,Target completed,Code Formatted

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,8 +1,6 @@
 class Todo < ActiveRecord::Base
-    def to_pleasant_string
-
-          is_completed = completed ? "[X]" : "[ ]" 
-          "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
-      
-    end
+  def to_pleasant_string
+    is_completed = completed ? "[X]" : "[ ]"
+    "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
+  end
 end
