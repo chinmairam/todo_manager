20215fdd5751d6710a4b0967e2ad1b7cd9704068
chinmairam <chinmairammuppa@gmail.com>
2020-04-16 07:20:20 +0530

    Add sign-out functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,4 +13,10 @@ class SessionsController < ApplicationController
       render plain: "Incorrect details!"
     end
   end
+
+  def destroy
+    session[:current_user_id] = nil
+    @current_user = nil
+    redirect_to "/"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   #post "users/login", to: "users#login"
   get "/signin" => "sessions#new", as: :new_sessions
   post "/signin" => "sessions#create", as: :sessions
+  delete "/signout" => "sessions#destroy", as: :destroy_session
 end
