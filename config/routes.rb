68d68246338b2c36425a389f2d0f073e025722bd
chinmairam <chinmairammuppa@gmail.com>
2020-04-16 10:19:51 +0530

    Level-9 Submission

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   get "/" => "home#index"
   resources :todos
   resources :users
-  #post "users/login", to: "users#login"
   get "/signin" => "sessions#new", as: :new_sessions
   post "/signin" => "sessions#create", as: :sessions
   delete "/signout" => "sessions#destroy", as: :destroy_session
